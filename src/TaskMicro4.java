3de24891751f7fca124814b7b5ae20cecde02dc3
Alexander <hezvashe@gmail.com>
2023-11-27 20:39:46 +0300

    Косметические изменения во второй задаче: убрана проверка ввода, изменено название переменных

--- src/TaskMicro4.java
+++ src/TaskMicro4.java
@@ -4,22 +4,17 @@ public class TaskMicro4 {
     public static void main(String[] args) {
         System.out.print("Введите n ");
         var in = new Scanner(System.in);
-        int n = 0;
-        var input = in.next();
-        try {
-            n = (int)Double.parseDouble(input); // проверка ввода на всякий случай
-        }
-        catch (NumberFormatException ignored) {}
-        in.close();
+        int n = in.nextInt();
+        //in.close();
 
         int prevNumber;
         var rnd = new Random();
-        int maxCombo = 0; // пока мы не уверены что хотя бы 1 число есть - комбо 0
+        int maxRepeats = 0; // пока мы не уверены что хотя бы 1 число есть - длина серии = 0
 
         if (n > 0) {
             int number = rnd.nextInt(2000) - 1000; // создадим первое заранее
-            int curCombo = 1;
-            maxCombo = 1; // мы ввели первое число, так что комбо автоматически стало равным 1
+            int curRepeats = 1;
+            maxRepeats = 1; // мы ввели первое число, так что комбо автоматически стало равным 1
 
             for (int i = 1; i < n; i++) {
                 prevNumber = number;
@@ -28,17 +23,17 @@ public class TaskMicro4 {
                 boolean currentNumberCombinesWithOld = (number * prevNumber > 0) || (number == 0 && prevNumber == 0);
 
                 if (currentNumberCombinesWithOld) {
-                    curCombo++;
-                    if (curCombo > maxCombo) {
-                        maxCombo = curCombo;
+                    curRepeats++;
+                    if (curRepeats > maxRepeats) {
+                        maxRepeats = curRepeats;
                     }
                 }
                 else {
-                    curCombo = 1;
+                    curRepeats = 1;
                 }
             }
         }
 
-        System.out.println("Самая длинная серия: " + maxCombo);
+        System.out.println("Самая длинная серия: " + maxRepeats);
     }
 }
